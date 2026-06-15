315261147fe952bc7b41f650b8f39a406e93c306
Ksu Shine <ksushine@Ksus-MacBook-Pro.local>
2026-06-15 09:26:14 +0200

    Split As well/So that, remove Well/I feel like, delete Actual vs card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'polski-v7';
+const CACHE = 'polski-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
