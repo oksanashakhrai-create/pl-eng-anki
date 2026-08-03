18add5118f40df1b3135bd0949a7c99761cc3984
Ksu Shine <ksushine@Ksus-MacBook-Pro.local>
2026-08-03 21:54:15 +0200

    Add 24 PL cards (Разное), 5 EN cards (Rie), edit 2 cards (v30)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'polski-v29';
+const CACHE = 'polski-v30';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
